c672ce01e3ec089d23cb2be292814979a759ca86
Oliver Andrews <oltandrews@gmail.com>
2023-02-15 09:23:13 -0800

    loginscreen

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,25 +1,20 @@
 "use client"
 import { signIn } from "next-auth/react";
 import Image from "next/image"
+import Box from "@mui/material/Box";
 
 function Login() {
   return (
-    <div className="bg-[#11A37F] flex flex-col h-screen 
+    <div className="bg-[#397EF7] flex flex-col text-white font-bold text-3xl h-screen w-screen 
     items-center justify-center text-center">
-      <Image 
-        src="https://links.papareact.com/2i6"
-        width={300}
-        height={300}
-        alt="logo"
-      />
-      <button onClick={() => signIn("google")} className="text-white font-bold 
-      text-3xl animate-pulse">
+      <Box display={"flex"} flexDirection="column" fontFamily="Poppins">
+        Atty Chat
+      <button onClick={() => signIn("google")} className=" 
+      animate-pulse">
         Sign In with Google
       </button>
-      <button onClick={() => signIn("email")} className="text-white font-bold 
-      text-3xl animate-pulse mt-4">
-        Sign In with Email
-      </button>
+      </Box>
+
     </div>
   )
 }
