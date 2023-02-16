bfaa463a01207cd124c5ab9555df33cf08ccd781
Oliver Andrews <oltandrews@gmail.com>
2023-02-16 12:34:37 -0800

    addedbuttons to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,65 @@
+"use client";
 import {
-    BoltIcon,
-    ExclamationTriangleIcon,
-    SunIcon,
-  } from "@heroicons/react/24/outline";
-  
-  function HomePage() {
-    return (
-      <div className="text-white flex flex-col h-screen px-2 items-center justify-center">
-        <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
-  
-        <div className="flex space-x-2 text-center">
-          <div>
-            <div className="flex justify-center flex-col items-center mb-5">
-              <SunIcon className="h-8 w-8" />
-              <h2>Examples</h2>
+  BoltIcon,
+  ExclamationTriangleIcon,
+  SunIcon,
+} from "@heroicons/react/24/outline";
+import { Box } from "@mui/material";
+import DrawerSpacer from "../components/DrawerSpacer";
+
+import NewChatWithMessage from "../components/NewChatWithMessage";
+
+function HomePage() {
+  return (
+<>
+      <Box fontFamily="poppins" className="bg-[#397EF7] h-screen fixed" width="100%" height="100%">
+        <DrawerSpacer />
+
+        <div className="text-white flex flex-col px-2 items-center justify-center">
+          <h1 className="text-5xl font-bold mb-10">AttyChat</h1>
+          <h1 className="text-3xl font-bold mb-5">Quick Questions</h1>
+
+          <div className="flex space-x-2 text-center">
+            <div>
+              <div className="flex justify-center flex-col items-center mb-5">
+                <SunIcon className="h-8 w-8" />
+                <h2>Examples</h2>
+                <div className="space-y-2">
+                  <NewChatWithMessage messageText="Remembers what user said earlier in the conversation" />
+                  <NewChatWithMessage messageText="Distinguishes between different entities and their attributes" />
+                  <NewChatWithMessage messageText="Generates human-like responses" />
+                </div>
+              </div>
             </div>
-  
-            <div className="space-y-2">
-              <p className="infoText">Explain something to me</p>
-              <p className="infoText">
-                "What is the difference between a dog and a cat?"
-              </p>
-              <p className="infoText">"What is the color of the sun?"</p>
+
+            <div>
+              <div className="flex justify-center flex-col items-center mb-5">
+                <BoltIcon className="h-8 w-8" />
+                <h2>Capabilities</h2>
+                <div className="space-y-2">
+                  <NewChatWithMessage messageText="Remembers what user said earlier in the conversation" />
+                  <NewChatWithMessage messageText="Distinguishes between different entities and their attributes" />
+                  <NewChatWithMessage messageText="Generates human-like responses" />
+                </div>
+              </div>
             </div>
-          </div>
-  
-          <div>
-            <div className="flex justify-center flex-col items-center mb-5">
-              <BoltIcon className="h-8 w-8" />
-              <h2>Capabilities</h2>
-            </div>
-  
-            <div className="space-y-2">
-              <p className="infoText">
-                Remembers what user said earlier in the conversation
-              </p>
-              <p className="infoText">
-                "What is the difference between a dog and a cat?"
-              </p>
-              <p className="infoText">"What is the color of the sun?"</p>
-            </div>
-          </div>
-  
-          <div>
-            <div className="flex justify-center flex-col items-center mb-5">
-              <ExclamationTriangleIcon className="h-8 w-8" />
-              <h2>Limitations</h2>
-            </div>
-  
-            <div className="space-y-2">
-              <p className="infoText">Explain something to me</p>
-              <p className="infoText">
-                "What is the difference between a dog and a cat?"
-              </p>
-              <p className="infoText">"What is the color of the sun?"</p>
+
+            <div>
+              <div className="flex justify-center flex-col items-center mb-5">
+                <ExclamationTriangleIcon className="h-8 w-8" />
+                <h2>Limitations</h2>
+                <div className="space-y-2">
+                  <NewChatWithMessage messageText="Remembers what user said earlier in the conversation" />
+                  <NewChatWithMessage messageText="Distinguishes between different entities and their attributes" />
+                  <NewChatWithMessage messageText="Generates human-like responses" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
-  }
-  
-  export default HomePage;
+      </Box>
+    </>
+  );
+}
+
+export default HomePage;
