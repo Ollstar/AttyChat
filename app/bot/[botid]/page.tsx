07637605e810d8ebad1da97acff7e98684192d68
Oliver Andrews <oltandrews@gmail.com>
2023-02-21 12:33:37 -0800

    botquestions

--- app/bot/[botid]/page.tsx
+++ app/bot/[botid]/page.tsx
@@ -1,9 +1,15 @@
 "use client"
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import DrawerSpacer from "../../../components/DrawerSpacer";
 import NewChatWithBot from "../../../components/NewChatWithBot";
 import { db } from "../../../firebase";
 import { doc, getDoc } from "firebase/firestore";
+import { useState, useEffect } from "react";
+
+type Bot = {
+  botName: string;
+  botQuestions: string[];
+};
 
 type Props = {
   params: {
@@ -12,35 +18,46 @@ type Props = {
 };
 
 function BotPage({ params: { botid } }: Props) {
+  const [bot, setBot] = useState<Bot | null>(null);
 
-  const getBot = async () => {
-    const docRef = doc(db, "bots", botid);
-    const docSnap = await getDoc(docRef);
+  useEffect(() => {
+    const getBot = async () => {
+      const docRef = doc(db, "bots", botid);
+      const docSnap = await getDoc(docRef);
 
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-    }
-  };
+      if (docSnap.exists()) {
+        setBot(docSnap.data() as Bot);
+      } else {
+        console.log("No such document!");
+        setBot(null);
+      }
+    };
 
-  getBot();
+    getBot();
+  }, [botid]);
+
+  if (!bot) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
-      <Box fontFamily="poppins" className="bg-[#397EF7]" width="100%">
+    <div className="bg-[#397EF7] h-screen w-screen">
+      <Box fontFamily="poppins" className="bg-[#397EF7]"  >
         <DrawerSpacer />
 
         <div className="text-white flex flex-col px-2 items-center justify-center">
-          <h1 className="text-5xl font-bold mb-10">AttyChat</h1>
+          <h1 className="text-5xl font-bold mb-10">{bot.botName}</h1>
           <h1 className="text-3xl font-bold mb-10">Quick Questions</h1>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 w-2/3 h-screen text-center">
-            <NewChatWithBot messageText="Hello" botid={botid} />
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 text-center">
+            {bot.botQuestions.map((question, index) => (
+              <NewChatWithBot key={index} messageText={question} botid={botid} />
+            ))}
           </div>
         </div>
       </Box>
+      </div>
     </>
   );
 }
