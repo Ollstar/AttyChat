72c8ebd71385d9ab441cb18662280d4d23844efc
Oliver Andrews <oltandrews@gmail.com>
2023-02-25 17:49:33 -0800

    bot screen

--- app/bot/[botid]/page.tsx
+++ app/bot/[botid]/page.tsx
@@ -52,7 +52,7 @@ function BotPage({ params: { botid } }: Props) {
   return (
     <>
         <Box fontFamily="poppins" sx={{backgroundColor:bot!.botColor, height:"100vhw",width:"100vhw"}}>
-          <div >
+          <div className={`h-screen w-screen bg-[${bot!.botColor}]`}>
           <DrawerSpacer/>
           <div className="text-white flex flex-col px-2 pb-4 items-center">
             <div className="flex flex-row">
